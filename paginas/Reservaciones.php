d8154af35549bc64d630fa616df40fb09cd90e47
Jossymare <jmr91_@hotmail.com>
2024-08-18 17:14:17 -0600

    ProyectoFinal-Version2

---
+++ paginas/Reservaciones.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<head>   <title> Reservaciones </title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+<link rel="stylesheet" type="text/css" href="../css/estilo.css"/>
+<link rel="stylesheet" type="text/css" href="../css/estilos3.css"/>
+<link rel="stylesheet" type="text/css" href="../css/estilos2.css"/>
+<link rel="stylesheet" type="text/css" href="../fonts.css"/>
+<link rel="stylesheet" type="text/css" href="../css/estilo4.css"/>
+<link rel="stylesheet" type="text/css" href="../css/final.css"/>
+<link rel="stylesheet" href="../css/styles.css">
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="header.js"></script>
+<script src="arriba.js"></script>
+</head>
+
+<body>
+<header>
+<div class="wrapper">
+<div class="logo">
+Restaurante DonGato
+</div>
+    <nav>
+        <div class="menu-icon" onclick="toggleMenu()">
+            &#9776; <!-- Icono de hamburguesa -->
+        </div>
+        <ul id="menu">
+            <li><a href="../Restaurante-DonGato.php">Inicio</a></li>
+            <li><a href="Platillos.php">Platillos y Postres</a></li>
+            <li><a href="Reservaciones.php">Reserva</a></li>
+        </ul>
+    </nav>
+</div>
+
+</header>
+
+
+<center>
+
+</table>
+<table>
+<tr><td colspan="2"><center><h1>San Jose </h1></center> </td> <br>
+</tr>
+
+<tr><td><p> El país y principalmente San José han experimentado un crecimiento y desarrollo comercial muy grande en la última década, por consecuente se han tenido que construir grandes centros comerciales para albergar el creciente comercio nacional.
+
+En los centros comerciales de la ciudad se pueden encontrar: tiendas de ropa, zapatos, electrónica de consumo, especializadas, por departamentos etc. Además de farmacias, bancos, supermercados, carnicerías, restaurantes, ferreterías, bazares, algunos cuentan con cines y áreas de juego para niños.</p> </td>
+ </td> <td> <img src="../img/r4.jpg"> </td> </tr>
+
+<tr><td> <img src="../img/r5.jpg"> </td> <td> <p> Es un área urbana de alrededor de 550 m de largo y 8200 m² de construcción, en una extensión de 7 cuadras de la ciudad: entre la avenida 2ª y avenida 14 (en dirección norte sur) y entre las calles 7 y 11 
+La zona comenzó a desarrollarse como un núcleo comercial de origen chino durante las últimas décadas del siglo XX, básicamente con la instalación de supermercados, tiendas y algunos restaurantes cuyos propietarios son de esa nacionalidad.
+
+El barrio fue oficialmente inaugurado el miércoles 5 de diciembre de 2012</p></td>
+</tr>
+
+ 
+ <tr><td colspan="2"><center><h1>Guanacaste </h1></center> </td>
+</tr>
+<tr><td colspan="2"><center><img src="../img/r6.jpg"></center> </td>
+</tr><tr><td colspan="2"> Tierra de grandes llanuras y haciendas ganaderas, ha producido costumbres y tradiciones relacionadas con estos aspectos. De herencia española es la corrida de toros, que se realiza en rústicos redondeles de madera, donde resalta la monta rústica al estilo guanacasteco, considerada la expresión autóctona más representativa de las tradiciones en Guanacaste, que se practica en los diferentes cantones de la provincia. La monta de toros al estilo guanacasteco se diferencia de otras montas porque se utiliza una espuela corrediza, así como la forma en que se coloca la correa que rodea el pecho del toro. Los topes y las carreras de cintas son también expresión cultural propia de esta provincia. En estas tradiciones, el caballo toma protagonismo como medio básico de transporte en una provincia de grandes extensiones planas. La destreza de cabalgar potros totalmente chúcaros, montados sin pretal, es otra tradición que aún persiste en Guanacaste</td>
+</tr> 
+ <tr><td colspan="2"><center><h1>Heredia </h1></center> </td>
+</tr>
+
+<tr><td> <img src="../img/r7.jpg"> </td> <td> <p>A diferencia de la carreta típica costarricense, que es pintada con un estilo particular y es de mayor influencia en el Valle Central Se distingue también por su ausencia de paredes, ya que a diferencia de la carreta típica costarricense, usada para transportar granos (café principalmente), la carreta guanacasteca se usa para el transporte de maderas, troncos y ramas. La pica'e leña es un tradicional desfile de carretas guanacastecas que se realiza anualmente en la ciudad de Nicoya, que marca el inicio de las festividades en honor a la Virgen de Guadalupe.</p>
+
+<tr><td><p>La ciudad surgió alrededor de una ayuda de parroquia a principios del siglo XVIII con el nombre de Cubujuquí. En 1763 se le dio la denominación oficial de villa de Heredia, pero el título de villa fue revocado en 1779. Se le conoció popularmente como Villa Vieja, pero esa práctica fue prohibida por las autoridades en 1801. Las Cortes españolas le concedieron nuevamente el título de villa en 1813. Se le conoce popularmente además como "La Ciudad de las Flores", el apellido Flores de una familia influyente, por su paisaje y sus hermosas mujeres.
+
+</p></td>
+ </td> <td> <img src="../img/r11.jpg"> </td> </tr>
+ 
+ <tr><td colspan="2"><center><h1>Puntarenas </h1></center> </td>
+</tr>
+
+<tr><td> <img src="../img/r12.jpg"> </td> <td><p> La ciudad de Puntarenas, capital de la provincia, es rica en tradición, historia y costumbres. Cuenta con diversos lugares de interés turístico, entre los que pueden mencionarse, además de la extensa playa, el Paseo de los Turistas, el Balneario de Barrio El Carmen, el boulevard de la catedral, el parque marino y la Casa de la Cultura. Uno de los sitios turísticos por excelencia es el Paseo de los Turistas, una avenida peatonal que bordea el océano, y en la cual se encuentra toda clase de negocios: hoteles, bares, heladerías, discotecas, etc.</p></td>
+</tr>
+
+
+</table>
+
+</center>
+
+<script src="../js/script.js"></script>
+
+<footer>
+<br>
+<br>
+<center> Buscanos en las Redes Sociales  
+<br>
+<br>
+<pre>Facebook <a href="https://www.facebook.com/"><body link="#fff" vlink="fff" alink="#fff"> <span class="icon-facebook2"></span></a> Twitter <a href="https://twitter.com/?lang=es">   <span class="icon-twitter"><body link="#fff" vlink="fff" alink="#fff"> <a/></span> </pre>
+<br>
+<span class="icon-home"></span>  Derechos Reservados por Jossymar Molina Rodriguez<br>
+<br>
+</center>
+</footer>
+</body>
+</html>
